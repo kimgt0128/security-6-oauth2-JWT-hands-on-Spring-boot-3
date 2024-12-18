83c5f72c693ae22acf5a14d39afd3582f90d0e87
KKT <kimgt2828@gmail.com>
2024-12-18 17:34:55 +0900

    feat: 유저 정보 저장을 위한 엔티티, 레포지토리, 서비스 구현 - OAuth2JwtMember: 유저 정보를 관리하는 엔티티 - MemberRepository: 유저 엔티티티 관리를 위한 레포지토리 - CustomOAuth2UserService: 유저 중복 검사 후 가입 or 로그인 로직 구현 -

--- src/main/java/com/wondrous/oauth2_JWT/service/CustomOAuth2UserService.java
+++ src/main/java/com/wondrous/oauth2_JWT/service/CustomOAuth2UserService.java
@@ -1,6 +1,9 @@
 package com.wondrous.oauth2_JWT.service;
 
 import com.wondrous.oauth2_JWT.dto.*;
+import com.wondrous.oauth2_JWT.entity.OAuth2JwtMember;
+import com.wondrous.oauth2_JWT.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
@@ -10,11 +13,17 @@ import org.springframework.security.oauth2.core.OAuth2Error;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
+@RequiredArgsConstructor
+
 @Service
 public class CustomOAuth2UserService extends DefaultOAuth2UserService {
 
+    private final MemberRepository memberRepository;
+
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+
+        // 1. 인가 서버 ㅇ
 
         OAuth2User oAuth2User = super.loadUser(userRequest);
 
@@ -46,14 +55,29 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
             throw new OAuth2AuthenticationException(new OAuth2Error("invalid_social_account", "잘못된 소셜 계정입니다.", null));
         }
 
-        // 우리 서버에서 담을 User Dto
+        // 이미 가입된 유저인지 확인 후 가입 or 로그인 처리
+
         //이름이 겹칠 상황을 대비하여 이름 + 리소스 서버 Id로 usernmae 생성
         String username = oAuth2User.getName() + " " + oAuth2Response.getProviderId();
+        OAuth2JwtMember existMember = memberRepository.findByUsername(username);
+
 
         UserDto userDto = new UserDto(
                 "ROLE_USER",
                 oAuth2Response.getName(),
                 username);
+
+        if (existMember == null) {
+            System.out.println("새 유저 회원 가입 중");
+            OAuth2JwtMember newMember  = OAuth2JwtMember.newMember(userDto);
+            memberRepository.save(newMember);
+
+        }
+        else {
+            System.out.println("인가 서버로부터 기존 유저 정보 업데이트 중");
+            existMember.updateMember(userDto);
+            memberRepository.save(existMember);
+        }
 
         return new CustomOAuth2User(userDto);
     }
