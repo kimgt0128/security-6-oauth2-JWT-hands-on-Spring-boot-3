0f2a7f3960d012e146f6aff3c82112000a78cadd
KKT <kimgt2828@gmail.com>
2024-12-23 21:58:12 +0900

    feat: RTR(Refresh Token Rotate) 방식 적용 - ReissueTokenResponse: access, refresh 토큰 응답 dto - ReissuController: ReissueTokenResponse Dto를 반환하도록 구현 - JwtTokenReissueService: access 토큰 재발급시 refresh도 재발급

--- src/main/java/com/wondrous/oauth2_JWT/jwt/controller/ReissuController.java
+++ src/main/java/com/wondrous/oauth2_JWT/jwt/controller/ReissuController.java
@@ -1,7 +1,9 @@
 package com.wondrous.oauth2_JWT.jwt.controller;
 
+import com.wondrous.oauth2_JWT.jwt.dto.ReissueTokenResponse;
 import com.wondrous.oauth2_JWT.jwt.service.JwtTokenProvider;
 import com.wondrous.oauth2_JWT.jwt.service.JwtTokenReissueService;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -22,10 +24,21 @@ public class ReissuController {
                           HttpServletResponse response) {
         System.out.println("reissu controller, refresh token: " + refreshToken);
 
-        String newAccessToken =  jwtTokenReissueService.reissue(refreshToken);
+        ReissueTokenResponse  reissueToken=  jwtTokenReissueService.reissue(refreshToken);
+
+        String newAccessToken = reissueToken.getAccessToken();
+        String newRefreshToken = reissueToken.getRefreshToken();
 
         response.setStatus(HttpServletResponse.SC_OK);
+        response.setHeader("access", newAccessToken);
+        response.addCookie(createCookie("refresh", newRefreshToken));
 
         return newAccessToken;
+    }
+
+    private Cookie createCookie(String key, String value) {
+        Cookie cookie = new Cookie(key, value);
+        cookie.setHttpOnly(true);
+        return cookie;
     }
 }
--- src/main/java/com/wondrous/oauth2_JWT/jwt/service/JwtTokenReissueService.java
+++ src/main/java/com/wondrous/oauth2_JWT/jwt/service/JwtTokenReissueService.java
@@ -1,5 +1,6 @@
 package com.wondrous.oauth2_JWT.jwt.service;
 
+import com.wondrous.oauth2_JWT.jwt.dto.ReissueTokenResponse;
 import com.wondrous.oauth2_JWT.jwt.filter.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -11,7 +12,7 @@ public class JwtTokenReissueService {
 
     private final JwtTokenProvider jwtTokenProvider;
 
-    public String reissue(String refreshToken) {
+    public ReissueTokenResponse reissue(String refreshToken) {
         System.out.println("refreshToken: " + refreshToken);
 
         if (refreshToken == null || refreshToken.isEmpty()) {
@@ -32,10 +33,12 @@ public class JwtTokenReissueService {
         String username = jwtTokenProvider.getUsername(refreshToken);
         String role = jwtTokenProvider.getRole(refreshToken);
 
+        // RTR(Refresh Token Rotate) 방식 적용
         System.out.println("reissued token created");
         String newAccessToken = jwtTokenProvider.createJwt("access", username, role,  600000L);
+        String newRefreshToken = jwtTokenProvider.createJwt("refresh", username, role, 86400000L);
 
-        return newAccessToken;
+        return new ReissueTokenResponse(newRefreshToken, newAccessToken);
 
     }
 
